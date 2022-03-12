1a6d0cadb2874bdc55201c8cb38102a613be6ba7
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-03-11 20:28:47 -0500

    star project

--- ratingstar/app/src/Star.js
+++ ratingstar/app/src/Star.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Star({
+function Star ({
     value,
     color,
     handleHover,
@@ -9,40 +9,30 @@ function Star({
     isFilled
 }) {
     if (!handleHover) {
-        return ( <
-            span className = "Star"
-            style = {
-                { color }
-            }
-            onMouseEnter = {
-                () => console.log("Hovered In")
-            }
-            onMouseLeave = {
-                () => console.log("Hovered Out")
-            }
-            onClick = {
-                () => alert("Clicked a star!")
-            } >
-            <
-            /span>
+        return (
+            <span 
+                className="Star"
+                style = {{ color}}
+                onMouseEnter= {() => console.log("Hovered In")}
+                onMouseLeave = {() => console.log("Hovered Out")}
+                onClick = {() => alert("Clicked a star!")}
+            >
+            {isFilled ? "★" : "☆"}
+            </span>
 
-        );
+            );
     }
 
-    return ( <
-        span className = "Star"
-        style = {
-            { color }
-        }
-        onMouseEnter = {
-            () => handleHover(value)
-        }
-        onMouseLeave = {
-            () => handleHoverLeave(value)
-        }
-        onClick = {
-            () => handleClick(value)
-        } > { isFilled ? "★" : "☆" } < /span>
+    return (
+        <span
+        className = "Star"
+        style = {{color}}
+        onMouseEnter = {() => handleHover(value)}
+        onMouseLeave = {() => handleHoverLeave(value)}
+        onClick = {() => handleClick(value)}
+        >
+         {isFilled ? "★" : "☆"}
+         </span>
     );
 }
 
--- ratingstar/app/src/index.js
+++ ratingstar/app/src/index.js
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Star from "./Star";
-
+import Rating from "./Rating";
 
 import "./styles.css";
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        h1 > Star Components: < /h1> < /
-        div >
+    return ( <div className = "App" >
+        <h1> Star Components: </h1> 
+        <Star color="pink" isFilled />
+        <Star color="magenta" isFilled />
+        <Star color="indigo" />
+         <h2>Rating Component (uses Stars):</h2>
+      <Rating stars={4} />
+      <Rating stars={5} />
+      <Rating stars={3} />
+
+        </div>
     );
 }
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
