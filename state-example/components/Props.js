d5cebf7a0ad41ef2c69268fdf35d8e89002bfa92
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-10-13 23:17:57 -0400

    Passing props to a component example

---
+++ state-example/components/Props.js
@@ -0,0 +1,18 @@
+function Avatar() {
+    return (
+        <img 
+            className="avatar"
+            src="https://i.imgur.com/1bX5QH6.jpg"
+            alt="Lin Lanying"
+            width={100}
+            eight={100}
+        />
+    )
+} 
+function Props() {
+  return (
+    <Avatar />
+  )
+}
+
+export default Props
---
+++ state-example/pages/learn/props-example.js
@@ -0,0 +1,9 @@
+import Props from "../../components/Props";
+
+
+function PropsExample() {
+    return(
+        <Props />
+    )
+}
+export default PropsExample;
