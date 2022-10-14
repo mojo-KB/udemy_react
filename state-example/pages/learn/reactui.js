7d849019eaace3a784efb611b5a0dc8b0f48138c
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-10-13 22:10:33 -0400

    create sameple components

---
+++ state-example/pages/learn/reactui.js
@@ -0,0 +1,12 @@
+import Avatar from "../../components/Avatar";
+import TodoList from "../../components/TodoList";
+import TodoListv2 from "../../components/TodoListv2";
+export default function React() {
+    return(
+        <div>
+            <Avatar />
+            <TodoList />
+            <TodoListv2 />
+        </div>
+    )
+}
