5f7f4b1209fa60029ac86fa1cb4178884183ee1f
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-10-15 20:38:55 -0400

    update files

--- state-example/components/Children.js
+++ state-example/components/Children.js
@@ -7,4 +7,37 @@ function getImageUrl (person, size = 's') {
     )
 }
 
-function 
+function Avatar({ person, size }) {
+    return (
+        <img 
+            className="avatar"
+            src={getImageUrl(person)}
+            alt={person.name}
+            width={size}
+            height={size}
+        />
+    )
+}
+
+function Card({ children }) {
+    return(
+        <div className="card">
+            {children}
+        </div>
+    )
+}
+
+
+export default function Children() {
+    return (
+        <Card>
+            <Avatar
+            size={100}
+            person={{ 
+            name: 'Katsuko Saruhashi',
+            imageId: 'YfeOqp2'
+            }}
+        />
+        </Card>
+    )
+}
