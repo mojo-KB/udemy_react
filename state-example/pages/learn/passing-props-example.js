97845c2d729beed640c0451ef153e01c040a7ab0
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-10-16 23:02:34 -0400

    update files

---
+++ state-example/pages/learn/passing-props-example.js
@@ -0,0 +1,14 @@
+import Gallery from "../../components/Gallery2";
+
+import React from 'react'
+
+function PassingProps() {
+  return (
+    <div>
+        <Gallery />
+    </div>
+  )
+}
+
+export default PassingProps 
+
