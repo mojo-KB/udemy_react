2a82e9e94a7b2420ffb517a2dfa6b8d3e31dfad5
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-11-09 01:51:11 -0500

    update files

---
+++ typescript/ts/basic.ts
@@ -0,0 +1,12 @@
+console.log("Hello")
+
+
+
+// create variable
+const hello: string = 'Hello World';
+let movieTitle: string = 'Amadeus';
+movieTitle = "Arrival";
+
+let myNumber: number = 42;
+const isTrue: boolean = true;
+
