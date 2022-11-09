a4f8b634aa1024570ad744043ffcd9ebf9af39c9
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-11-09 13:36:48 -0500

    update files

--- typescript/ts/basic.ts
+++ typescript/ts/basic.ts
@@ -1,6 +1,4 @@
-console.log("Hello")
-
-
+const Hello: Console =console.log("Hello")
 
 // create variable
 const hello: string = 'Hello World';
@@ -9,4 +7,14 @@ movieTitle = "Arrival";
 
 let myNumber: number = 42;
 const isTrue: boolean = true;
+
+// Type Inference
+
+
+// Any Type
+let thing: any = 'hello';
+
+thing = 1;
+thing = false;
+thing()
 
