e7a7d0e9668bae60a2c0a8a89b35d2bb3b6cba19
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-11-11 18:27:01 -0500

    update files

--- typescript/ts/object.ts
+++ typescript/ts/object.ts
@@ -33,3 +33,62 @@ function createUser(user: {
 }) {
     return "Hello";
 }
+
+
+// Type Alias; usually for Object type
+type Point = {
+    x: number;
+    y: number
+}
+
+let secondCoordinate: Point = {x: 34, y: 2};
+
+function returnCoordinate(): Point {
+    return {x: Math.random(), y: Math.random()}
+}
+
+function doublePoint(inputPoint: Point): Point {
+    return {x: inputPoint.x * 2, y: inputPoint.x * 2};
+}
+
+type myNum = number; 
+
+
+// Nested Objects
+const describePerson = (person: {
+    name: string;
+    age: number;
+    parentNames: {
+        mom: string;
+        dad: string;
+    }
+}): string => {
+    return `
+    Person: ${person.name},
+    Age: ${person.age},
+    Parents: ${person.parentNames.mom}, ${person.parentNames.dad}
+    `
+}
+
+describePerson({
+    name: 'Jimmy', 
+    age: 19,
+    parentNames: {
+        mom: 'kim',
+        dad: "steve"
+    }
+})
+
+const Song1 = {
+    title: 'Unchained Melody',
+    artist: "Rgitheous Brothers",
+    numStreams: 12345678,
+    credits: { 
+        producer: "Phil Spector",
+        writer: "Alex North"
+    }
+}
+function calculatePayou() {
+
+}
+
