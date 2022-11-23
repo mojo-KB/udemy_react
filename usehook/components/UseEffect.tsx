561ad21cf2a562794b945cdd397a67806bfaeaa5
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-11-22 19:16:48 -0500

    update files

--- usehook/components/UseEffect.tsx
+++ usehook/components/UseEffect.tsx
@@ -2,31 +2,39 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 type Props = {}
 
-function UseEffect({}: Props) {
-    const [windowWidth, setWindowWidth]  = useState(window.innerWidth)
-    const [name, setName] = useState<string>("")
+function UseEffect({ }: Props) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+  const [name, setName] = useState<string>("")
 
 
-    // useEffect perform side effects on a functional component
+  // useEffect perform side effects on a functional component
 
-    // Class Components : use life cycle methods
-    /*
-    componentDidMount() : Lifecycle function when component mounts/loads
+  // Class Components : use life cycle methods
+  /*
+  componentDidMount() : Lifecycle function when component mounts/loads
 
-    componentDidUpdaet() : Lifecycle functions wehn components props change
+  componentDidUpdaet() : Lifecycle functions wehn components props change
 
-    componentWillUnmount() : Lifecycle functions when components unmounts/cleanup functions
-    */
-    // Functional Components : useEffect hook
-
-
-    // On Every render - note that it doesn't have dependencies
-    useEffect(() => {
-        window.addEventListener('resize', updateWindowWidth)
-    })
+  componentWillUnmount() : Lifecycle functions when components unmounts/cleanup functions
+  */
+  // Functional Components : useEffect hook
 
 
-    
+  // On Every render - note that it doesn't have dependencies
+  useEffect(() => {
+    console.log("I re-rendered")
+  }, [])
+
+
+  useEffect(() => {
+    window.addEventListener('resize', updateW)
+  })
+
+
+
+
+
+
 
   return (
     <div>UseEffect</div>
