04ef2e74ecff22c3df92922e32a5301de57d6c11
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-12-25 20:31:56 -0500

    update file

---
+++ react-docs-v2/pages/mananing_state.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function mananing_state({ }: Props) {
+    return (
+        <div className='bg-black text-white'>mananing_state</div>
+    )
+}
+
+export default mananing_state
