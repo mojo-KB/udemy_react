614d9994238a0ef894092e936cc3fe81c85794e6
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2023-01-12 03:36:33 -0500

    update file

--- typescript/ts/classSetup.ts
+++ typescript/ts/classSetup.ts
@@ -2,6 +2,16 @@
 
 
 class Person {
+    // class fields
+    first: string;
+    last: string;
+    score: number = 0;
+    numLives: number = 10;
+
+    // private field
+
+
+
     // constructor(name: string, age:number) {
     //     this.name = name;
     //     this.age = age;
@@ -9,6 +19,15 @@ class Person {
     // greet() {
     //     return `Hello ${this.name}`
     // }
+   
+    
+    // constructors
+    constructor (first: string, last: string) {
+        this.first = first;
+        this.last = last;
+    }
+
+    // functions
     taunt() {
         return "BOOYAH!"
     } 
@@ -16,8 +35,9 @@ class Person {
 
 
 //const jimmy = new Person('Jimmy', 25)
-const player1 = new Person();
-console.log(player1.taunt());
+const player2 = new Person("Bunla", "Kour")
+//const player1 = new Person();
+console.log(player2.taunt());
 
 
 
