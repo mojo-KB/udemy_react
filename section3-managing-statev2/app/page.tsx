15387de46060ac2d2ab18e9afc01a534b5941e31
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2023-01-30 12:18:29 -0500

    update file

--- section3-managing-statev2/app/page.tsx
+++ section3-managing-statev2/app/page.tsx
@@ -2,9 +2,16 @@
 
 export default function Home() {
   return (
-    <div className='bg-black text-white'>
-      <div>hello</div>
-      <div>world</div>
+    <div className='bg-black text-white flex flex-col'>
+      <div>Managing State</div>
+      <div>Key concepts</div>
+      <div>Reacting to input with state</div>
+      <div>Chosing the state structure</div>
+      <div>Sharing state between components</div>
+      <div>Preserving and resetting state</div>
+      <div>Extracting state logic into a Reducer</div>
+      <div>Passing Data Deeply with context</div>
+      <div>Scaling up with Reducer and Context</div>
     </div>
   )
 }
